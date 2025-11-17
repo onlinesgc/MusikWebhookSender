41013a39dbd74111ad11c8e5883e6af9114457b3
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-11-17 13:06:53 +0100

    added saveing for not sending more messages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { WebhookClient } from 'discord.js';
 import dotenv from 'dotenv';
 import { setDefaultResultOrder } from 'dns';
+import fs from 'fs';
 setDefaultResultOrder('ipv4first');
 dotenv.config();
 
@@ -13,16 +14,24 @@ const webhookClient = new WebhookClient({ url: webhookUrl! });
 
 const sentTimestamps = [] as string[];
 
+if(!fs.existsSync("sentTimestamps.json")){
+    fs.writeFileSync("sentTimestamps.json", JSON.stringify(sentTimestamps));
+} else {
+    const fileData = fs.readFileSync("sentTimestamps.json", "utf-8");
+    sentTimestamps.push(...JSON.parse(fileData));
+}
+
 setInterval(async () => {
     const res = await fetch("https://musikapi.lukasabbe.com/api/collection/" + bossa);
     const data = await res.json();
     for (const donator of data.donators){
         if(!sentTimestamps.includes(donator.timestamp)){
-            const message = `Tack så mycket till ${donator.name == "" ? "En hemlig hjälte" : donator.name} som har donerat ${donator.amount} kr till SGCs bössa i Musikhjälpen!`;
+            const message = `Tack så mycket till ${donator.name == "" ? "En hemlig hjälte" : donator.name} som har donerat ${donator.amount} kr till SGCs bössa i Musikhjälpen! Totalt har vi nu samlat ihop ${data.amount} kr!`;
             await webhookClient.send({
                 content: message
             });
             sentTimestamps.push(donator.timestamp);
+            fs.writeFileSync("sentTimestamps.json", JSON.stringify(sentTimestamps));
         }
     }
 }, 1000 * 30); // Check every 30 seconds
