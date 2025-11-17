85270fececda71681ebd0bec07263b8430578122
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-11-17 11:18:42 +0100

    Musikhjalpen message sender

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { WebhookClient } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const bossa = "sgc-musikhjaelpen"
+
+const webhookUrl = process.env.WEBHOOK_URL;
+
+const webhookClient = new WebhookClient({ url: webhookUrl! });
+
+const sentTimestamps = [] as string[];
+
+setInterval(async () => {
+    const res = await fetch("https://musikapi.lukasabbe.com/api/collection/" + bossa);
+    const data = await res.json();
+    for (const donator of data.donators){
+        if(!sentTimestamps.includes(donator.timestamp)){
+            const message = `Tack så mycket till ${donator.name == "" ? "En hemlig hjälte" : donator.name} som har donerat ${donator.amount} kr till SGCs bössa i Musikhjälpen!`;
+            await webhookClient.send({
+                content: message
+            });
+            sentTimestamps.push(donator.timestamp);
+        }
+    }
+}, 1000 * 30); // Check every 30 seconds
